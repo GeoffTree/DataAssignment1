ef0d3afe2ac837ea685373feacaf0cd320733c3b
Geoffrey Trenholm <trenholg@sheridancollege.ca>
2025-01-17 20:13:55 -0500

    bunch of non-assignment changes in cmake-build files?

---
+++ cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
@@ -0,0 +1,4 @@
+set -e
+
+cd /cygdrive/c/Users/geoff/projects/DataProject1/cmake-build-debug
+/cygdrive/c/Users/geoff/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
---
+++ cmake-build-debug/CMakeFiles/rebuild_cache.dir/utility.sh
@@ -0,0 +1,4 @@
+set -e
+
+cd /cygdrive/c/Users/geoff/projects/DataProject1/cmake-build-debug
+/cygdrive/c/Users/geoff/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
